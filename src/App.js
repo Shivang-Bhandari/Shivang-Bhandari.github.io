87d31e00311571770c4a2b4fa43200b6d9592c93
Shivang Bhandari <pump382156@gmail.com>
2020-04-20 00:16:40 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React, { useState } from 'react';
 import {themes, ThemeContext} from './ThemeContext'; 
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar';
+import Home from './components/Home';
 import './App.scss';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      theme: themes.light
-    };
-
-    this.toggleTheme  = () => {
-      const currentTheme = this.state.theme;
-      const themeToBeApplied = this.state.theme === themes.light ? themes.dark : themes.light;
-      this.setState({
-        theme: themeToBeApplied
-      }, console.log('theme has been toggled', this.state.theme));
-    }
   }
-
-
-
   render() {
     return (
       <div className="App">
         <Sidebar />
+        <Home />
       </div>
     );
   }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <span>Hi, I'm Shivang Bhandari.</span>
+      <br />
+      <span>A Front-End Engineer</span>
+      <span>Currently working at Haptik.</span>
+    </div>
+  );
+}
+
+export default Home;
